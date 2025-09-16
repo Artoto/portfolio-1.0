9a5f70f91760a6df41001c9f8faf6888c9dc8aeb
Artoto <88313738+Arthit0@users.noreply.github.com>
2025-09-16 15:27:32 +0700

    add dropdown resume thai and english.

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -1,4 +1,20 @@
+"use client";
+
+import React, { useState } from "react";
+
 const HeroSection: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 500);
+  };
+
   return (
     <section id="hero">
       <div className="max-w-7xl w-full mx-auto flex flex-col items-start justify-center min-h-screen text-center p-6 text-gray-400">
@@ -11,29 +27,55 @@ const HeroSection: React.FC = () => {
           building high-performance, scalable, and responsive web apps
           solutions.`}
         </p>
-        <button
-          onClick={() => {
-            const link = document.createElement("a");
-            link.href = "/Resume.pdf"; // Make sure to place your PDF in the public folder
-            link.download = "arthit-resume.pdf";
-            link.click();
-          }}
-          className="mt-6 px-6 py-3 bg-purple-500 hover:bg-transparent hover:border hover:border-purple-500 hover:text-purple-500 text-black font-bold group pointer-events-auto relative flex h-fit w-fit items-center justify-center overflow-hidden rounded-lg  uppercase tracking-wide"
-        >
-          <span className="absolute inset-0 z-1 block overflow-hidden">
-            <span className="block h-full w-full translate-y-full rounded-t-[15rem] bg-[var(--color-accent-600)] transition-all duration-500 ease-[cubic-bezier(0.77,0,0.175,1)] sm:group-hover:translate-y-0 sm:group-hover:rounded-none"></span>
-          </span>
-          <span className="relative z-2 block overflow-hidden transition-all">
-            <span
-              className="block after:absolute after:left-0 after:block after:translate-y-0 after:transition-all after:duration-500 after:ease-[cubic-bezier(0.77,0,0.175,1)] after:content-[attr(data-after)] sm:group-hover:after:-translate-y-full"
-              data-after="Download resume"
-            >
-              <span className="flex transition-all duration-500 ease-[cubic-bezier(0.77,0,0.175,1)] sm:group-hover:-translate-y-full">
-                {`Download resume`}
+
+        <div className="relative h-auto" onMouseEnter={handleMouseEnter}>
+          <button
+            className={`mt-6 px-6 py-3  hover:bg-transparent hover:border hover:border-purple-500 hover:text-purple-500 ${
+              isOpen
+                ? "bg-transparent border border-purple-500 text-purple-500"
+                : " bg-purple-500 text-black "
+            } font-bold group pointer-events-auto relative flex h-fit w-fit items-center justify-center overflow-hidden rounded-lg  uppercase tracking-wide`}
+          >
+            <span className="relative z-2 block overflow-hidden transition-all">
+              <span
+                className="block after:absolute after:left-0 after:block after:translate-y-0 after:transition-all after:duration-500 after:ease-[cubic-bezier(0.77,0,0.175,1)] after:content-[attr(data-after)] sm:group-hover:after:-translate-y-full"
+                data-after="Download resume"
+              >
+                <span className="flex transition-all duration-500 ease-[cubic-bezier(0.77,0,0.175,1)] sm:group-hover:-translate-y-full">
+                  {`Download resume`}
+                </span>
               </span>
             </span>
-          </span>
-        </button>
+          </button>
+
+          {isOpen && (
+            <div
+              className="absolute right-0 mt-2 w-full  rounded-md shadow-lg py-1 z-10"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <a
+                href="/Resume.pdf"
+                target="_blank"
+                download="Resume.pdf"
+                className="block px-4 py-2 bg-purple-500 text-black hover:bg-transparent hover:border hover:border-purple-500 hover:text-purple-500"
+                onClick={handleMouseLeave}
+              >
+                {`Resume (Thai)`}
+              </a>
+              <a
+                href="/Resume-Eng.pdf"
+                target="_blank"
+                download="Resume-Eng.pdf"
+                className="block px-4 py-2 bg-purple-500 text-black hover:bg-transparent hover:border hover:border-purple-500 hover:text-purple-500"
+                onClick={handleMouseLeave}
+              >
+                {`Resume (English)`}
+              </a>
+            </div>
+          )}
+        </div>
+
         <div className=" absolute bottom-4 md:bottom-40 right-12 md:right-16 flex flex-col gap-7 md:gap-10 text-right">
           <div className="flex flex-col">
             <span className="text-3xl md:text-6xl font-bold text-purple-500">
